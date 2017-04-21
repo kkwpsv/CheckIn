d01b021b975fab1b71a17b02062f0f9f60f58d8a
HarborZeng <harbourzeng@gmail.com>
2017-04-21 16:32:51 +0800

    加了好多网页
    累死我了

---
+++ CheckInManager/Controllers/LocationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheckIn.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheckIn.Manager.Controllers
+{
+    public class LocationController :ControllerWithAuthorize
+    {
+        public LocationController(CheckInContext context) : base(context)
+        {
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CheckInManager/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheckIn.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheckIn.Manager.Controllers
+{
+    public class UsersController :ControllerWithAuthorize
+    {
+        public UsersController(CheckInContext context) : base(context)
+        {
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
